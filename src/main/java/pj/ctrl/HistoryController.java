9031b8481e4d479b020b429c617efd1c82ba4700
xukun <493501286@qq.com>
2016-06-23 15:49:56 +0800

    history bug 修复

--- src/main/java/pj/ctrl/HistoryController.java
+++ src/main/java/pj/ctrl/HistoryController.java
@@ -16,6 +16,8 @@ import pj.support.Utils;
 
 @RestController
 public class HistoryController {
+	
+	/*增加一个用户的搜索记录*/
 	@RequestMapping("/history/{user_id}/{history_content}")
     public Ret addUserHistory(@PathVariable("user_id")String user_id, @PathVariable("history_content")String history_content,
     		HttpServletRequest request,HttpServletResponse response){
@@ -34,12 +36,12 @@ public class HistoryController {
 		statement = "mapping.historyMapper.addHistory";
 		int add = session.insert(statement, new History(-1, user_id, history_content));
 		session.commit();
-		System.out.println(add);
 		
 		return new Ret(add);
 	}
 	
-	@RequestMapping("/history/{user_id}")
+	/*获取一个用户的所有记录*/
+	@RequestMapping("/history/get/{user_id}")
 	public List<History> getUserHistory(@PathVariable("user_id")String user_id,
 			HttpServletRequest request,HttpServletResponse response){
 		response.setHeader("Access-Control-Allow-Origin", "*");
@@ -50,7 +52,8 @@ public class HistoryController {
 		return list;
 	}
 	
-	@RequestMapping("/history/{history_id}")
+	/*删除某个搜索记录*/
+	@RequestMapping("/history/delete/{history_id}")
 	public Ret deleteUserHistory(@PathVariable("history_id")int history_id,
 			HttpServletRequest request,HttpServletResponse response){
 		response.setHeader("Access-Control-Allow-Origin", "*");
@@ -58,7 +61,20 @@ public class HistoryController {
 		
 		String statement = "mapping.historyMapper.deleteHistory";
 		int delete = session.delete(statement, history_id);
-		System.out.println(delete);
+		session.commit();
 		return new Ret(delete);
+	}
+	
+	/*删除某个用户的搜索记录*/
+	@RequestMapping("/history/deleteAll/{user_id}")
+	public Ret deleteOneUserAll(@PathVariable("user_id")String user_id,
+			HttpServletRequest request,HttpServletResponse response){
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		SqlSession session = Utils.getSession();
+		
+		String statement = "mapping.historyMapper.deleteUserHistory";
+		int delete = session.delete(statement, user_id);
+		session.commit();
+		return new Ret(delete>0?1:0);
 	}
 }
